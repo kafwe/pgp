4ebadddb50319e1d0d1480c4bc603ae31f3b0e1e
Matthew Craig <crgmat002@myuct.ac.za>
2024-05-10 13:39:32 +0200

    Simplified PGP further

--- confidentiality/pgp.py
+++ confidentiality/pgp.py
@@ -7,22 +7,23 @@ SECRET_lENGTH_BYTES = 2
 
 # `data` represents the concatenated encrypted message and secret key
 def pgp_decrypt(data: bytes, private_key: PrivateKey) -> bytes:
-    encrypted_secret, encrypted_message = _split(data)
-    secret = SecretKey(private_key.decrypt(encrypted_secret))
-    return secret.decrypt(encrypted_message)
+    e_secret, e_message = _split(data)
+    secret = SecretKey(private_key.decrypt(e_secret))
+    return secret.decrypt(e_message)
 
 
+# Returns bytes representing the concatted secret length, encrypted secret, and encrypted message
 def pgp_encrypt(message: bytes, peer_public_key: PublicKey) -> bytes:
     secret = SecretKey()
-    encrypted_secret = peer_public_key.encrypt(secret.kiv)
-    encrypted_message = secret.encrypt(message)
-    secret_length = len(encrypted_secret).to_bytes(SECRET_lENGTH_BYTES, byteorder="big")
-    return secret_length + encrypted_secret + encrypted_message
+    e_secret = peer_public_key.encrypt(secret.kiv)
+    e_message = secret.encrypt(message)
+    secret_len = len(e_secret).to_bytes(SECRET_lENGTH_BYTES, byteorder="big")
+    return secret_len + e_secret + e_message
 
 
 def _split(data: bytes) -> tuple[bytes, bytes]:
-    secret_length = int.from_bytes(data[:SECRET_lENGTH_BYTES], byteorder="big")
+    secret_len = int.from_bytes(data[:SECRET_lENGTH_BYTES], byteorder="big")
     data = data[SECRET_lENGTH_BYTES:]
-    encrypted_secret = data[:secret_length]
-    encrypted_message = data[secret_length:]
-    return encrypted_secret, encrypted_message
+    e_secret = data[:secret_len]
+    e_message = data[secret_len:]
+    return e_secret, e_message
