4d595639a65bad3514f325fe8c947fa172d4591a
Matthew Craig <crgmat002@myuct.ac.za>
2024-05-11 14:31:40 +0200

    Fixed message sending bugs

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import socket
 import threading
 import communication.communication as comms
+import communication.messages as msg
+import log
 
 
 def main():
+    log.configure()
     choice = input("Do you want to host (1) or to connect (2): ")
 
     if choice == "1":
@@ -13,33 +15,9 @@ def main():
     else:
         exit()
 
-    threading.Thread(target=comms.sending_messages, args=(client,)).start()
-    threading.Thread(target=comms.receiving_messages, args=(client,)).start()
+    threading.Thread(target=msg.sending_messages, args=(client,)).start()
+    threading.Thread(target=msg.receiving_messages, args=(client,)).start()
 
 
 if __name__ == "__main__":
     main()
-
-### Structure of the Message ###
-""" A caption + the image data encoded as a string
-
-So the app must first prompt the user to select/name a file.
-Then it must prompt them to write a caption for the image.
-
-HeaderSize is fixed at 11 bytes - 3 bytes for caption length, 8 bytes for image data length
-"""
-
-
-def start_server() -> socket.socket:
-    # AF_INET specifies IPV4, SOCK_STREAM specifies a TCP connection
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((socket.gethostname(), 9999))
-    server.listen()
-    client, _ = server.accept()
-    return client
-
-
-def start_client(ip_address: str = socket.gethostname()) -> socket.socket:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((ip_address, 9999))
-    return client
